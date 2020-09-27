b0012a6e9e48603f0a09dfec2f1db5c73cad40b9
Rajesh Sagayanathan <68595140+Rajesh-a11y@users.noreply.github.com>
2020-09-27 12:45:07 +0200

    Create NewYearNewMe.swift

---
+++ arrays-and-sets/NewYearNewMe.swift
@@ -0,0 +1,8 @@
+var resolutions = ["play more music 🎸",
+                   "read more books 📚",
+                   "drink more water 💧"]
+
+resolutions.append("cook more food 🍝")
+resolutions += ["save more money 💰"]
+
+print(resolutions)
